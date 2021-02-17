c547f5ede465accff7b7e3d08c4c7fe5ace6ebda
RainerMauersberger23 <829532@mystma.org>
2021-02-17 16:22:43 +0000

    Planets 1

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-#WRITE YOUR CODE IN THIS FILE
+def planets(p):
+    for i in range (0, len(planet)):
+        if p == planets[i]:
+
+
+planets = ["mercury", "venus", "earth", "mars", "jupiter", "saturn", "uranus", "neptune"]
+print (planets("earth"))
